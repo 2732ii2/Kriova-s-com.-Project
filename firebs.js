8d96361c7bc58171b0bdae26c26625c59feb5256
Ashad Mubeen <mo.anas123khan@gmail.com>
2022-08-13 13:31:44 +0530

    commit_a

---
+++ firebs.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAo7C2f1I2Hente1mPBdG6nyYUvPC0gEMg",
+  authDomain: "kriovaproject.firebaseapp.com",
+  databaseURL: "https://kriovaproject-default-rtdb.firebaseio.com",
+  projectId: "kriovaproject",
+  storageBucket: "kriovaproject.appspot.com",
+  messagingSenderId: "649178855387",
+  appId: "1:649178855387:web:ce52acea92f9e896ce19fe",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
